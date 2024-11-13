05159c5e967a962a31aa32f5f2a6898f2c6ffd8e
Mega Nugraha <monmega110@gmail.com>
2024-11-13 17:43:13 +0700

    Enhance Firebase initialization: add error handling for service account path and improve emulator setup logic

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser"); // Middleware to parse incoming reque
 const routes = require("./routes"); // Custom routes for handling user-related requests
 
 // Load environment variables from .env file
-const path = require("path");
 require("dotenv").config();
+const path = require("path");
 
 // Firebase Admin SDK initialization
 const { initializeApp, cert } = require("firebase-admin/app");
@@ -19,29 +19,37 @@ const PORT = process.env.PORT || 7070;
 // Use a hard-coded flag to toggle emulator
 const USE_FIRESTORE_EMULATOR = false; // Set to `false` for deployment
 
-if (!USE_FIRESTORE_EMULATOR) {
-  // Initialize Firebase app with service account credentials from cloudrun
-  initializeApp();
-} else {
-  // Firebase service account credentials
-  const serviceAccountPath = process.env.SERVICE_ACCOUNT_PATH;
-  const serviceAccount = require(serviceAccountPath);
-  // Initialize Firebase app with service account credentials
-  console.log("Using Provided Credentials...");
-  initializeApp({
-    credential: cert(serviceAccount),
-  });
-}
+try {
+  if (!USE_FIRESTORE_EMULATOR) {
+    // Initialize Firebase app with service account credentials from cloudrun
+    initializeApp();
+  } else {
+    // Firebase service account credentials
+    const serviceAccountPath = process.env.SERVICE_ACCOUNT_PATH;
+    if (!serviceAccountPath) {
+      throw new Error("SERVICE_ACCOUNT_PATH environment variable is not set.");
+    }
+    const serviceAccount = require(serviceAccountPath);
+    // Initialize Firebase app with service account credentials
+    console.log("Using Provided Credentials...");
+    initializeApp({
+      credential: cert(serviceAccount),
+    });
+  }
 
-// Set Firestore instance as a global variable
-global.db = getFirestore();
+  // Set Firestore instance as a global variable
+  global.db = getFirestore();
 
-if (USE_FIRESTORE_EMULATOR) {
-  console.log("Using Firestore emulator...");
-  db.settings({
-    host: "localhost:8080", // Firestore emulator host
-    ssl: false, // Disable SSL for the emulator connection
-  });
+  if (USE_FIRESTORE_EMULATOR) {
+    console.log("Using Firestore emulator...");
+    db.settings({
+      host: "localhost:8080", // Firestore emulator host
+      ssl: false, // Disable SSL for the emulator connection
+    });
+  }
+} catch (error) {
+  console.error("Error initializing Firebase:", error);
+  process.exit(1); // Exit the process with an error code
 }
 
 const app = express(); // Initialize Express app
